56cdf3f64d43bbd571a8f91987653b493bc9a35a
GAMMJ <gammjmj@gmail.com>
2026-02-03 13:45:41 +0900

    fix: App.jsx return문 수정

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,14 @@
 // 오른쪽 페이지 새로고침을 누르면 결과가 반영됩니다.
 function App() {
   let count = 3;
-  return 
+  return (
+    <>
       <h1>React는 JSX 문법을 사용합니다. </h1>
       <p>총 3가지 오류가 있어요. </p>
       <p>자바스크립트 요소를 사용하려면 </p>
-      <p> count+count = count + count 이렇게 하면 됩니다.</p>
-      // count + count = 6 이렇게 나와야합니다.
+      <p> count+count = {count + count} 이렇게 하면 됩니다.</p>
+      {/* count + count = 6 이렇게 나와야합니다. */}
+    </>
+  )
 }
 export default App;
